8b33677cb4cf64f71c3bf3e00608e01cf02a6860
Rachit Patel <rachitp509@gmail.com>
2023-12-31 22:07:35 +0530

    updated content

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import "./Experience.scss";
 
@@ -20,16 +20,16 @@ const Experience = () => {
             <span>Bhumi</span>
           </button>
           <button
-            onClick={() => setActive(1)}
-            className={`${active === 1 ? "active" : ""}`}
-          >
-            <span>SIH 2022</span>
-          </button>
-          <button
             onClick={() => setActive(2)}
             className={`${active === 2 ? "active" : ""}`}
           >
             <span> Kenvent IT</span>
+          </button>
+          <button
+            onClick={() => setActive(1)}
+            className={`${active === 1 ? "active" : ""}`}
+          >
+            <span>QuantumIT Innovation</span>
           </button>
         </div>
         <div className="roles_responsive">
@@ -40,16 +40,16 @@ const Experience = () => {
             <span>Bhumi</span>
           </button>
           <button
-            onClick={() => setActive(1)}
-            className={`${active === 1 ? "active" : ""}`}
-          >
-            <span>SIH 2022</span>
-          </button>
-          <button
             onClick={() => setActive(2)}
             className={`${active === 2 ? "active" : ""}`}
           >
             <span> Kenvent IT</span>
+          </button>
+          <button
+            onClick={() => setActive(1)}
+            className={`${active === 1 ? "active" : ""}`}
+          >
+            <span>QuantumIT Innovation</span>
           </button>
         </div>
         <div className="position">
@@ -97,67 +97,6 @@ const Experience = () => {
               </div>
             </div>
           )}
-          {active === 1 && (
-            <div className="container">
-              <div className="head">
-                <div className="first">
-                  <h3>Backend & Frontend Developer</h3>
-                  <a className="first_a" href="https://www.sih.gov.in/">
-                    <p>@</p>
-                    <p>SMART INDIA HACKATHON 2022</p>
-                  </a>
-                  <a className="first_a_res" href="https://www.sih.gov.in/">
-                    <p>@</p>
-                    <p>SIH 2022</p>
-                  </a>
-                </div>
-                <div className="second">
-                  <span>November 2021</span> <span>-</span>{" "}
-                  <span>April 2022</span>
-                </div>
-              </div>
-              <div className="desc">
-                <ol>
-                  <li>
-                    <div>•</div>
-                    <div>
-                      Smart India Hackathon is a{" "}
-                      <span>nationwide initiative</span> to provide students
-                      with a platform to solve some of the{" "}
-                      <span>pressing problems</span> we face in our daily lives.
-                    </div>
-                  </li>
-                  <li>
-                    <div>•</div>
-                    <div>
-                      Our Team consists of 6 members and we are working on a
-                      project named <span>Smart Learning</span>. I'm working as
-                      a <span>Backend & Frontend Developer</span> in this
-                      project.
-                    </div>
-                  </li>
-                  <li>
-                    <div>•</div>
-                    <div>
-                      Our Problem Statement was <span>Smart Education</span> and
-                      we have to develop a <span>Software</span> through which
-                      we can provide <span>Interactive</span> &{" "}
-                      <span>Interactive</span> Learning.
-                    </div>
-                  </li>
-                  <li>
-                    <div>•</div>
-                    <div>
-                      Though we could not make it to the <span>final list</span>{" "}
-                      but we learned a lot from this experience. We are Proud of
-                      our Team and looking forward to face{" "}
-                      <span>more challenges</span> in future.
-                    </div>
-                  </li>
-                </ol>
-              </div>
-            </div>
-          )}
           {active === 2 && (
             <div className="container">
               <div className="head">
@@ -183,8 +122,8 @@ const Experience = () => {
                     <div>•</div>
                     <div>
                       Kenvent Services is one of the best{" "}
-                      <span>website design</span>{" "}
-                      development companies in <span>India</span>.
+                      <span>website design</span> development companies in{" "}
+                      <span>India</span>.
                     </div>
                   </li>
                   <li>
@@ -205,6 +144,62 @@ const Experience = () => {
                     <div>•</div>
                     <div>
                       Handle the <span>updation</span> of the work each day.
+                    </div>
+                  </li>
+                </ol>
+              </div>
+            </div>
+          )}
+          {active === 1 && (
+            <div className="container">
+              <div className="head">
+                <div className="first">
+                  <h3>Fullstack Intern</h3>
+                  <a
+                    className="first_a"
+                    href="https://quantumitinnovation.com/"
+                  >
+                    <p>@</p>
+                    <p>QuantumIT Innovation</p>
+                  </a>
+                </div>
+                <div className="second">
+                  <span>September 2023</span> <span>-</span>{" "}
+                  <span>Ongoing</span>
+                </div>
+              </div>
+              <div className="desc">
+                <ol>
+                  <li>
+                    <div>•</div>
+                    <div>
+                      Participate in the design and development of various{" "}
+                      <span>web-based</span> applications using
+                      <span>ReactJS</span>,<span> Node.js</span>,
+                      <span> JavaScript</span> , and other relevant programming
+                      languages.
+                    </div>
+                  </li>
+                  <li>
+                    <div>•</div>
+                    <div>
+                      Develop new user-facing features using{" "}
+                      <span>ReactJS</span>.
+                    </div>
+                  </li>
+                  <li>
+                    <div>•</div>
+                    <div>
+                      Work on <span>NodeJS</span>,<span>ExpressJS</span> and
+                      creating <span>RESTful APLs</span> in Backend.
+                    </div>
+                  </li>
+                  <li>
+                    <div>•</div>
+                    <div>
+                      Work on <span>Redux</span>,<span>React hooks</span>,
+                      <span>state Management</span>, and React component
+                      lifecycle.
                     </div>
                   </li>
                 </ol>
